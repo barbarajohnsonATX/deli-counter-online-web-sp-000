de4d57c15c54cb2494f73ab9c9bf4c8b3e90c101
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-05 02:42:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 def line(deli)
-  if !deli.length 
+  if deli.length == 0
     puts "The line is currently empty."
   else
     puts "The line is currently:"  
     deli.each_with_index do |name, index| 
-      puts "#{index+1}. #{name}"
+      puts "#{index}. #{name}"
     end 
   end 
 
